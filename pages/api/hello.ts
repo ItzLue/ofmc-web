5a910edc9e116b167d4f5bb3498657604a9b4b49
Loui Nissen-Petersen <s194294@student.dtu.dk>
2022-05-31 16:58:45 +0200

    Docker and anb file

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
+import * as shell from 'child_process';
 
-type Data = {
-  name: string
-}
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {
+        const log = shell.execSync("ofmc ./public/tls.AnB --numSess 2", {encoding: 'utf8'});
+        return res.status(200).json({message: log.toString()});
+    }
+    return res.status(500).json({message: 'Method not allowed'});
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default handler;
